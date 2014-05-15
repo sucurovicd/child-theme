e688d3511a6d9664a815d0dbf6e7717162b24afb
sucurovicd <skull_serbia@yahoo.com>
2014-05-15 10:43:16 +0200

    Levi sidebar,breadcrumb, izgled proizvoda

--- responsive-child/functions.php
+++ responsive-child/functions.php
@@ -5,6 +5,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//Sklanjanje komentara
+add_filter( 'woocommerce_product_tabs', 'sb_woo_remove_reviews_tab', 98);
+function sb_woo_remove_reviews_tab($tabs) {
+ unset($tabs['reviews']);
+ return $tabs;
+}
+
+
 register_sidebar(array(
     'name' => 'Shop left sidebar',
     'id' => 'shopleftsidebar',
@@ -12,7 +20,6 @@ register_sidebar(array(
     'before_widget' => '<div class="widget-shop-left-sidebar">',
     'after_widget' => '</div>',
 ));
-
 function get_root_parent($page_id) {
 global $wpdb;
     $parent = $wpdb->get_var("SELECT post_parent FROM $wpdb->posts WHERE post_type='page' AND ID = '$page_id'");
@@ -27,13 +34,10 @@ function url_prepare($word){
 function Get_All_Wordpress_Menus(){
     return get_terms( 'nav_menu', array( 'hide_empty' => true ) ); 
 }
-
 //Izlistava sve podmenije jednog menija
 function Get_sub_menus($name){
     return wp_get_nav_menu_items($name);
 }
-
-
 function nav_bar($page_id){
     //Izvlacimo pocetne roditelje
     $root = get_root_parent($page_id);
@@ -52,13 +56,11 @@ function nav_bar($page_id){
                 <?php
                 if( has_nav_menu( 'sub-header-menu', 'responsive' ) ) { ?>
 			<?php wp_nav_menu( array(
-								   'container'      => '',
-								   'menu_class'     => 'sub-header-menu',
-								   'theme_location' => 'sub-header-menu'
-							   )
-			);
-			?>
-		<?php }
+			'container'      => '',
+			'menu_class'     => 'sub-header-menu',
+			'theme_location' => 'sub-header-menu'
+		));
+		}
                 ?>
                 </ul>
             </div>
@@ -100,4 +102,26 @@ function nav_bar($page_id){
         }
     }
 }
-
+//Funkcija za izlistavanje side meni-a
+function side_nav_menu($page_id){
+    if(!is_woocommerce()){
+        $svi_roditelji = get_post_ancestors($page_id); 
+        end($svi_roditelji);
+        $drugi = prev($svi_roditelji);
+        //Ako ne postoji drugi, dodeljujemo mu vrednost
+        if (empty($drugi))
+            $drugi = $page_id;
+        $args = array(
+            'child_of' => $drugi,
+            'depth' => 0,
+            'sort_column' => 'post_date',
+            'title_li'=> get_the_title($drugi),
+            'echo' => 0,
+        );
+        //Dajemu mu izlistavanje
+           $meni = wp_list_pages($args);
+           echo "<div class='side_menu'>";
+            echo $meni;
+           echo '</div>';
+    }
+  }
---
+++ responsive-child/sidebar-left.php
@@ -0,0 +1,32 @@
+<?php
+
+// Exit if accessed directly
+if( !defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Main Widget Template
+ *
+ *
+ * @file           sidebar-left.php
+ * @package        Responsive
+ * @author         Emil Uzelac
+ * @copyright      2003 - 2014 CyberChimps
+ * @license        license.txt
+ * @version        Release: 1.0
+ * @filesource     wp-content/themes/responsive/sidebar-left.php
+ * @link           http://codex.wordpress.org/Theme_Development#Widgets_.28sidebar.php.29
+ * @since          available since Release 1.0
+ */
+?>
+<?php get_responsive_breadcrumb_lists();
+?>
+<?php responsive_widgets_before(); // above widgets container hook ?>
+
+        <div class="col-300 fit">
+            <?php
+            side_nav_menu($post->ID);
+            ?>
+        </div>
+<?php responsive_widgets_after(); // after widgets container hook ?>
