42d0eba2e15c3bcecdcc5650d28a2d64c56350cf
sucurovicd <skull_serbia@yahoo.com>
2014-05-23 12:50:15 +0200

    Logo postavljen
    Logo - ako je front page, crne boje,ako ne, bele

--- responsive-child/header.php
+++ responsive-child/header.php
@@ -88,19 +88,16 @@ if( !defined( 'ABSPATH' ) ) {
 
 		<?php responsive_in_header(); // header hook ?>
 
-		<?php if( get_header_image() != '' ) : ?>
+
+		<?php if( is_front_page() ) : ?>
 
 			<div id="logo">
-				<a href="<?php echo home_url( '/' ); ?>"><img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php bloginfo( 'name' ); ?>"/></a>
+                            <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo dirname(get_bloginfo('stylesheet_url'))?>/images/logo.png"></a>
 			</div><!-- end of #logo -->
 
-		<?php endif; // header image was removed ?>
-
-		<?php if( !get_header_image() ) : ?>
-
+                <?php else:?>
 			<div id="logo">
-				<span class="site-name"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
-				<span class="site-description"><?php bloginfo( 'description' ); ?></span>
+                            <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo dirname(get_bloginfo('stylesheet_url'))?>/images/logo_foundation.png"></a>
 			</div><!-- end of #logo -->
 
 		<?php endif; // header image was removed (again) ?>
